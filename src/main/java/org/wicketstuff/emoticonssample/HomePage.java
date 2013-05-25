e18aefd178c0feb810c923008602671f445c0a38
andunslg <andunslg@gmail.com>
2013-05-25 23:12:23 +0530

    Changing the code to use new emoticons wicket-jquery-ui plugin

--- src/main/java/org/wicketstuff/emoticonssample/HomePage.java
+++ src/main/java/org/wicketstuff/emoticonssample/HomePage.java
@@ -1,20 +1,19 @@
 package org.wicketstuff.emoticonssample;
 
+import com.googlecode.wicket.jquery.ui.plugins.emoticons.EmotIconsBehavior;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
-import org.wicketstuff.emoticons.EmotIconsBehavior;
+
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-	    EmotIconsBehavior emotIconsBehavior=new EmotIconsBehavior();
-		emotIconsBehavior.addEmoticonize(".class");
-		emotIconsBehavior.addUnemoticonize(".class2");
-		emotIconsBehavior.addUnemoticonize("#id2");
-		emotIconsBehavior.addEmoticonize("#id3");
-		emotIconsBehavior.addUnemoticonize("#id4");
-		this.add(emotIconsBehavior);
+		this.add(new EmotIconsBehavior(".class","emoticonize"));
+		this.add(new EmotIconsBehavior(".class2","unemoticonize"));
+		this.add(new EmotIconsBehavior("#id2","unemoticonize"));
+		this.add(new EmotIconsBehavior("#id3","emoticonize"));
+		this.add(new EmotIconsBehavior("#id4","unemoticonize"));
     }
 }
